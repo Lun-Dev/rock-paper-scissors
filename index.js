65d9d15f066743106905a90d5b532a06e8e7ed69
Lun-Dev <lunthedev@gmail.com>
2021-09-14 02:32:08 +0800

    Modified index.js

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-let computerSelection = null
+let selection = ["Rock", "Paper", "Scissors"];
 
 function computerPlay() {
-    computerSelection = Math.floor(Math.random() * 3) // Math.floor() returns an integer, Math.random() returns a random floating point
+    let rand = Math.random();
+    let totalSelection = selection.length;
+    let randSelection = Math.floor(rand * totalSelection);
+    let computerSelection = selection[randSelection];
+    // computerSelection = Math.floor(Math.random() * 3) // Math.floor() returns an integer, Math.random() returns a random floating point
     switch(computerSelection) {
-        case 0:
-            computerSelection = "Rock";
+        case "Rock":
             document.getElementById("computer-choice").innerHTML = computerSelection;
             document.getElementById("result-field").innerHTML = "Computer Win";
             break;
-        case 1:
-            computerSelection = "Paper";
+        case "Paper":
             document.getElementById("computer-choice").innerHTML = computerSelection;
             document.getElementById("result-field").innerHTML = "Computer Lose";
             break;
-        case 2:
-            computerSelection = "Scissor";
+        case "Scissors":
             document.getElementById("computer-choice").innerHTML = computerSelection;
             document.getElementById("result-field").innerHTML = "Computer Win";
             break;
     }
 }
-
-
