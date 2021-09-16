e1ed16cc5df0c673075236d9bb1938fbdc9f4a24
Lun-Dev <lunthedev@gmail.com>
2021-09-16 22:42:47 +0800

    Modified: index.html, index.js

--- index.js
+++ index.js
@@ -1,24 +1,104 @@
-let selection = ["Rock", "Paper", "Scissors"];
-let randomChoice = selection[Math.floor(Math.random() * selection.length)]
+/* Global Variable */
+
+
+// let playerSelection = ""
+// let computerScore = 0
+// let playerScore = 0
+
+/* Player's Choice */
+
+// function rockChoice() {
+//     playerSelection = "Rock"
+//     result()
+// }
+
+// function paperChoice() {
+//     playerSelection = "Paper"
+//     result()
+// }
+
+// function scissorsChoice() {
+//     playerSelection = "Scissors"
+//     result()
+// }
+
+/* Computer's Choice */
+
+
+
+/* Result's Logic */
+
+
+/* Your game is going to play against the computer, 
+so begin with a function called 
+computerSelection that will randomly return either 
+‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in 
+the game to make the computer’s play. Tip: use the console 
+to make sure this is returning the expected output before 
+moving to the next step! */
+
+/* Global Variable */
+
+let playerSelection = ""
+let computerSelection = ["Rock", "Paper", "Scissors"]
+let playerChoiceMsg = "Player's Choice: "
+let computerChoiceMsg = "Computer's Choice: "
+let playerChoice = document.getElementById("playerChoiceEl")
+let computerChoice = document.getElementById("computerChoiceEl")
+
+/* Computer's Choice */
 
 function computerPlay() {
-    let rand = Math.random();
-    let totalSelection = selection.length;
-    let randSelection = Math.floor(rand * totalSelection);
-    let computerSelection = selection[randSelection];
-    // computerSelection = Math.floor(Math.random() * 3) // Math.floor() returns an integer, Math.random() returns a random floating point
-    switch(computerSelection) {
-        case "Rock":
-            document.getElementById("computer-choice").innerHTML = computerSelection;
-            document.getElementById("result-field").innerHTML = "Computer Win";
-            break;
-        case "Paper":
-            document.getElementById("computer-choice").innerHTML = computerSelection;
-            document.getElementById("result-field").innerHTML = "Computer Lose";
-            break;
-        case "Scissors":
-            document.getElementById("computer-choice").innerHTML = computerSelection;
-            document.getElementById("result-field").innerHTML = "Computer Win";
-            break;
+    let randomChoice = Math.floor(Math.random() * computerSelection.length)
+    computerSelection = computerSelection[randomChoice]
+    return computerSelection
+}
+
+/* Player's Choice */
+
+function rockChoice() {
+    playerSelection = "Rock"
+    render()
+}
+
+function paperChoice() {
+    playerSelection = "Paper"
+    render()
+}
+
+function scissorsChoice() {
+    playerSelection = "Scissors"
+    render()
+}
+
+/* Play Logic */
+function render() {
+    computerPlay()
+    playRound(playerSelection, computerSelection)
+    playerChoice.textContent = playerChoiceMsg + playerSelection
+    computerChoice.textContent = computerChoiceMsg + computerSelection
+}
+
+/* Main Logic */
+
+function playRound(playerSelection, computerSelection) {
+    if (computerSelection === "Rock" && playerSelection === "Paper") {
+        console.log("Player Win")
+    } else if (computerSelection === "Paper" && playerSelection === "Scissors") {
+        console.log("Player Lose")
+    } else if (computerSelection === "Scissors" && playerSelection === "Rock") {
+        console.log("Player Win")
+    } else if (computerSelection === "Rock" && playerSelection === "Scissors") {
+        console.log("Player Lose")
+    } else if (computerSelection === "Paper" && playerSelection === "Rock") {
+        console.log("Player Win")
+    } else if (computerSelection === "Scissors" && playerSelection === "Paper") {
+        console.log("Player Lose")
+    } else if (computerSelection === "Rock" && playerSelection === "Rock") {
+        console.log("Draw!")
+    } else if (computerSelection === "Paper" && playerSelection === "Paper") {
+        console.log("Draw!")
+    } else if (computerSelection === "Scissors" && playerSelection === "Scissors") {
+        console.log("Draw!")
     }
 }
