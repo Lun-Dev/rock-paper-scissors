e9598d53e736e0c915844cc6a3932bbbe18e453f
Lun-Dev <lunthedev@gmail.com>
2021-09-17 02:10:12 +0800

    Modified: index.js

--- index.js
+++ index.js
@@ -1,104 +1,28 @@
-/* Global Variable */
-
-
-// let playerSelection = ""
-// let computerScore = 0
-// let playerScore = 0
-
-/* Player's Choice */
-
-// function rockChoice() {
-//     playerSelection = "Rock"
-//     result()
-// }
-
-// function paperChoice() {
-//     playerSelection = "Paper"
-//     result()
-// }
-
-// function scissorsChoice() {
-//     playerSelection = "Scissors"
-//     result()
-// }
-
-/* Computer's Choice */
-
-
-
-/* Result's Logic */
-
-
-/* Your game is going to play against the computer, 
-so begin with a function called 
-computerSelection that will randomly return either 
-‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in 
-the game to make the computer’s play. Tip: use the console 
-to make sure this is returning the expected output before 
-moving to the next step! */
-
-/* Global Variable */
-
-let playerSelection = ""
-let computerSelection = ["Rock", "Paper", "Scissors"]
-let playerChoiceMsg = "Player's Choice: "
-let computerChoiceMsg = "Computer's Choice: "
-let playerChoice = document.getElementById("playerChoiceEl")
-let computerChoice = document.getElementById("computerChoiceEl")
-
-/* Computer's Choice */
-
 function computerPlay() {
-    let randomChoice = Math.floor(Math.random() * computerSelection.length)
-    computerSelection = computerSelection[randomChoice]
-    return computerSelection
+    let randomSelection = Math.floor(Math.random() * computerSelection.length)
+    return computerSelection = computerSelection[randomSelection]
 }
-
-/* Player's Choice */
-
-function rockChoice() {
-    playerSelection = "Rock"
-    render()
-}
-
-function paperChoice() {
-    playerSelection = "Paper"
-    render()
-}
-
-function scissorsChoice() {
-    playerSelection = "Scissors"
-    render()
-}
-
-/* Play Logic */
-function render() {
-    computerPlay()
-    playRound(playerSelection, computerSelection)
-    playerChoice.textContent = playerChoiceMsg + playerSelection
-    computerChoice.textContent = computerChoiceMsg + computerSelection
-}
-
-/* Main Logic */
 
 function playRound(playerSelection, computerSelection) {
-    if (computerSelection === "Rock" && playerSelection === "Paper") {
-        console.log("Player Win")
-    } else if (computerSelection === "Paper" && playerSelection === "Scissors") {
-        console.log("Player Lose")
-    } else if (computerSelection === "Scissors" && playerSelection === "Rock") {
-        console.log("Player Win")
-    } else if (computerSelection === "Rock" && playerSelection === "Scissors") {
-        console.log("Player Lose")
-    } else if (computerSelection === "Paper" && playerSelection === "Rock") {
-        console.log("Player Win")
-    } else if (computerSelection === "Scissors" && playerSelection === "Paper") {
-        console.log("Player Lose")
-    } else if (computerSelection === "Rock" && playerSelection === "Rock") {
-        console.log("Draw!")
-    } else if (computerSelection === "Paper" && playerSelection === "Paper") {
-        console.log("Draw!")
-    } else if (computerSelection === "Scissors" && playerSelection === "Scissors") {
-        console.log("Draw!")
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+     if (playerSelection === "rock" && computerSelection === "paper" || 
+        playerSelection === "paper" && computerSelection === "scissors" ||
+        playerSelection === "scissors" && computerSelection === "rock") {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`
+    } else if (playerSelection === "rock" && computerSelection === "scissors" || 
+               playerSelection === "paper" && computerSelection === "rock" ||
+               playerSelection === "scissors" && computerSelection === "paper") {
+        return `You Lose! ${playerSelection} beats ${computerSelection}`
+    } else {
+        return "Draw"
     }
 }
+
+let playerSelection = "Rock"
+let computerSelection = ["Rock", "Paper", "Scissors"];
+
+console.log(playerSelection)
+console.log(computerPlay())
+console.log(playRound(playerSelection, computerSelection))
+
